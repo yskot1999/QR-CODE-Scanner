1c421644e143f5b5f6ebff7cca55be3667646840
Yash Kothadiya <kyash1999@gmail.com>
2021-05-05 01:33:56 +0530

    Integrate QR code Scanner

---
+++ qrcode.js
@@ -0,0 +1,28 @@
+const Jimp = require("jimp");
+const fs = require('fs')
+const qrCode = require('qrcode-reader');
+async function scanner(url)
+{
+    let res='';
+    const buffer = fs.readFileSync(__dirname + '/uploads/' +  url);
+    try{
+        const image = await Jimp.read(buffer);
+        let qrcode = new qrCode();
+        qrcode.callback = function(err, value) {
+            if (err) {
+                console.error(err);
+            }
+            console.log(value.result);
+            res=value.result;
+            console.log(res);
+        };
+        qrcode.decode(image.bitmap);
+        return res;
+    }
+    catch(err){
+        console.log(err)
+        throw err;
+    }
+}
+
+module.exports =scanner;
