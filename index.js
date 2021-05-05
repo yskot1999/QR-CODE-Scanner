416d15e4911b153d0cc99c14c9d424c055ce18bf
Yash Kothadiya <kyash1999@gmail.com>
2021-05-05 16:28:23 +0530

    Improved frontend and Backend integration

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 const express=require("express");
 const multer = require('multer');
 const scanner=require('./qrcode');
+const path=require('path');
 const app = express();
 const PORT=process.env.PORT|| 4000;
 app.listen(PORT,() => console.log(`server running on port ${PORT}`));
-
-var path='';
+app.use('/script.js', express.static(path.join(__dirname, 'script.js')));
+app.use('/style.css', express.static(path.join(__dirname, 'style.css')))
+var pathnm='';
 var storage =   multer.diskStorage({  
     destination: function (req, file, callback) {  
       callback(null, './uploads');  
     },  
     filename: function (req, file, callback) {  
       callback(null, file.originalname);  
-      path=file.originalname;
-      //console.log(path);
+      pathnm=file.originalname;
+      console.log(pathnm);
     }  
   });  
-var upload = multer({ storage : storage}).single('myfile');  
+var upload = multer({ storage : storage}).single('myFile');  
     
 app.get('/',function(req,res){  
         res.sendFile(__dirname + "/index.html");  
 });  
     
-app.post('/upload',function(req,res){  
-    console.log(path);
+app.post('/upload', function(req,res){  
+    console.log(pathnm,"hello");
+    
     upload(req,res,async function(err) {  
+        console.log(pathnm)
         if(err) {  
             return res.end("Error uploading file.");  
         }  
         try{
-            const out=await scanner(path);
+            console.log(pathnm,"finally");
+            const out=await scanner(pathnm);
+            //res.end("hellop----");
             res.json({"success" : true,'output': out});
         }  
         catch(err){
+            console.log(err);
             res.json({"success" : false,'err': err});
         }
     });  
+    
+    
 });  
--- qrcode.js
+++ qrcode.js
@@ -1,9 +1,11 @@
 const Jimp = require("jimp");
 const fs = require('fs')
 const qrCode = require('qrcode-reader');
+
 async function scanner(url)
 {
     let res='';
+    console.log(__dirname + '/uploads/' +  url);
     const buffer = fs.readFileSync(__dirname + '/uploads/' +  url);
     try{
         const image = await Jimp.read(buffer);
